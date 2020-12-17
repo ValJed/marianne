fb0c1e35e823fb23b05593a6efb75759432ca618
Val <valentin.jeudy@gmail.com>
2020-12-17 23:33:31 +0100

    fix

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const minTop = 30
 tears[0].style.top = `-${Math.floor(Math.random() * (maxTop - minTop + 1) + minTop)}px`
 tears[1].style.top = `-${Math.floor(Math.random() * (maxTop - minTop + 1) + minTop)}px`
 
-// tears[0].style.top = `-30px`
-// tears[1].style.top = `-100px`
-// tears[2].style.top = `-70px`
-// tears[3].style.top = `-130px`
-
 setInterval(() => {
   tears.forEach((tear) => {
     const tearTop = parseInt(tear.style.top, 10)
@@ -21,7 +16,6 @@ setInterval(() => {
     if (tearTop > faceHeight) {
       const randomHeight = Math.floor(Math.random() * (maxTop - minTop + 1) + minTop);
 
-      console.log('randomHeight ===> ', randomHeight)
       tear.style.top = `-${randomHeight}px`
     } else {
       tear.style.top = `${tearTop + 2}px`
